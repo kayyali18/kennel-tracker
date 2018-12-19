3f8f16dbcdb7b0882b1a64176b2aca2c317e17b0
Laura Shamus <lkshamus@gmail.com>
2018-12-19 15:38:00 -0700

    Add bookingPage container

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -14,6 +14,7 @@ import styled from 'styled-components'
 
 import Header from 'components/Header'
 import HomePage from 'containers/HomePage/Loadable'
+import BookingPage from 'containers/BookingPage'
 import NotFoundPage from 'containers/NotFoundPage/Loadable'
 import BookingForm from 'components/BookingForm'
 import ProgressBar from 'components/ProgressBar'
@@ -39,6 +40,7 @@ export default function App() {
       <BookingForm />
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/booking" component={BookingPage} />
         <Route component={NotFoundPage} />
       </Switch>
       <GlobalStyle />
