b2d7338405f128e4f09856615c67ccd1f11e7599
Ahmad Kayyali <kayyali18@gmail.com>
2018-12-18 18:02:47 -0700

    Adjust Form to be liquid add a little CSS

---
+++ app/components/BookingForm/Form1.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const Form1 = styled.form`
+  position: absolute;
+  width: 100%;
+  background-color: beige;
+  height: 100%;
+
+  input {
+    border: 2px solid black;
+  }
+`
+export default Form1
+
+// make them checkboxes so when checked they can move to the next
