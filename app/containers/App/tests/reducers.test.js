28940c6387b528510c568984224e8e19903eaa8f
Laura Shamus <lkshamus@gmail.com>
2018-12-19 18:03:47 -0700

    Add reducer tests

---
+++ app/containers/App/tests/reducers.test.js
@@ -0,0 +1,40 @@
+import { fromJS } from 'immutable'
+
+import appReducer from '../reducer'
+import { loadApi, apiLoaded, apiLoadingError } from '../actions'
+
+describe('appReducer', () => {
+  let state
+  beforeEach(() => {
+    state = fromJS({
+      loading: false,
+      error: false,
+      dogData: false,
+      searchQuery: '',
+    })
+  })
+  it('should return the initial state', () => {
+    const expectedResult = state
+    expect(appReducer(undefined, {})).toEqual(expectedResult)
+  })
+  it('should handle the loadApi action correctly', () => {
+    const expectedResult = state
+      .set('loading', true)
+      .set('error', false)
+      .set('dogData', false)
+
+    expect(appReducer(state, loadApi())).toEqual(expectedResult)
+  })
+  it('should handle the apiLoaded action correctly', () => {
+    const results = 'Pasta'
+    const expectedResult = state.set('loading', false).set('dogData', results)
+
+    expect(appReducer(state, apiLoaded(results))).toEqual(expectedResult)
+  })
+  it('should handle the apiLoadingError action correctly', () => {
+    const error = true
+    const expectedResult = state.set('error', true).set('loading', false)
+
+    expect(appReducer(state, apiLoadingError(error))).toEqual(expectedResult)
+  })
+})
