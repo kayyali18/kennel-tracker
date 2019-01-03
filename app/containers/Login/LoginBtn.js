50cde7eaa46bfc73e7ce1028587c0d4ed936397e
Laura Shamus <lkshamus@gmail.com>
2019-01-03 14:43:25 -0700

    Add styling for button login

--- app/containers/Login/LoginBtn.js
+++ app/containers/Login/LoginBtn.js
@@ -10,14 +10,46 @@ const LoginBtn = styled.input.attrs({
   margin: props => props.size || '1em',
   padding: props => props.size || '1em',
 })`
-  color: palevioletred;
-  font-size: 1em;
-  border: 2px solid palevioletred;
-  border-radius: 3px;
+  display: inline-block;
+  position: relative;
+  outline: none;
+  padding: 17px 40px;
+  top: 0;
+  font-size: 30px;
+  font-family: "Open Sans", Helvetica;
+  border-radius: 4px;
+  border-bottom: 1px solid black;
+  background: #0e0b12;
+  color: #fff;
 
-  /* here we use the dynamically computed props */
-  margin: ${props => props.margin};
-  padding: ${props => props.padding};
+  -webkit-transform: translateZ(0);
+     -moz-transform: translateZ(0);
+      -ms-transform: translateZ(0);
+          transform: translateZ(0);
+  
+  -webkit-transition: all 0.2s ease;
+     -moz-transition: all 0.2s ease;
+      -ms-transition: all 0.2s ease;
+          transition: all 0.2s ease;
+}
+
+&:hover {
+  top: -10px;
+  box-shadow: 0px 10px 10px rgba( 15, 165, 60, 0.2 );
+  
+  -webkit-transform: rotateX(20deg);
+     -moz-transform: rotateX(20deg);
+      -ms-transform: rotateX(20deg);
+          transform: rotateX(20deg);
+}
+
+&:active {
+  color: black;
+  top: 0px;
+  box-shadow: 0px 0px 0px rgba( 15, 165, 60, 0.0 );
+  background: white;
+}
+
 `
 
 export default LoginBtn
