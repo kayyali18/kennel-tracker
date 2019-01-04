89ee412afba7f4d0556c5bc5bd09a1a3ccf6adf0
Ahmad Kayyali <kayyali18@gmail.com>
2019-01-04 08:05:07 -0700

    Uncomment some stuff

--- app/containers/BookingPage/index.js
+++ app/containers/BookingPage/index.js
@@ -1,101 +1,101 @@
-// /*
-// * Booking Page
-// * Here we allow clients to book new customers
-// */
+/*
+* Booking Page
+* Here we allow clients to book new customers
+*/
 
-// import React from 'react'
-// import PropTypes from 'prop-types'
-// import { Helmet } from 'react-helmet'
-// import { connect } from 'react-redux'
-// import { compose } from 'redux'
-// import { createStructuredSelector } from 'reselect'
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Helmet } from 'react-helmet'
+import { connect } from 'react-redux'
+import { compose } from 'redux'
+import { createStructuredSelector } from 'reselect'
 
-// import BookingForm from 'components/BookingForm'
-// import injectReducer from 'utils/injectReducer'
-// import injectSaga from 'utils/injectSaga'
-// import { DAEMON } from 'utils/constants'
-// // import Loading from 'components/Loading'
-// import Header from 'components/Header'
-// import saga from './saga'
-// import Wrapper from './Wrapper'
-// import {
-//   runPostSagaWatcher,
-//   postDog,
-//   postOwner,
-//   postVet,
-//   setPath,
-// } from './actions'
-// import reducer from './reducer'
-// import { makeSelectPath } from './selectors'
+import BookingForm from 'components/BookingForm'
+import injectReducer from 'utils/injectReducer'
+import injectSaga from 'utils/injectSaga'
+import { DAEMON } from 'utils/constants'
+// import Loading from 'components/Loading'
+import Header from 'components/Header'
+import saga from './saga'
+import Wrapper from './Wrapper'
+import {
+  runPostSagaWatcher,
+  postDog,
+  postOwner,
+  postVet,
+  setPath,
+} from './actions'
+import reducer from './reducer'
+import { makeSelectPath } from './selectors'
 
-// /* eslint-disable react/prefer-stateless-function */
+/* eslint-disable react/prefer-stateless-function */
 
-// export class BookingPage extends React.PureComponent {
-//   constructor() {
-//     super()
-//     this.state = {
-//       stage: 1,
-//       stageNames: ['owners', 'pets', 'vets'],
-//       path: 'owners',
-//     }
-//   }
+export class BookingPage extends React.PureComponent {
+  constructor() {
+    super()
+    this.state = {
+      stage: 1,
+      stageNames: ['owners', 'pets', 'vets'],
+      path: 'owners',
+    }
+  }
 
-//   saveForm = inputs => {
-//     const { dispatchSaga, dispatchPath } = this.props
-//     let { stage, stageNames } = this.state
-//     let path = stageNames[stage]
-//     dispatchSaga()
-//     dispatchPath(path)
+  saveForm = inputs => {
+    const { dispatchSaga, dispatchPath } = this.props
+    let { stage, stageNames } = this.state
+    let path = stageNames[stage]
+    dispatchSaga()
+    dispatchPath(path)
 
-//     stage += 1
-//     path = stageNames[stage]
+    stage += 1
+    path = stageNames[stage]
 
-//     // dispatchSaga()
+    // dispatchSaga()
 
-//     this.setState({
-//       postData: { ...inputs },
-//       path,
-//       stage,
-//     })
-//   }
+    this.setState({
+      postData: { ...inputs },
+      path,
+      stage,
+    })
+  }
 
-//   render() {
-//     const { stage } = this.state
-//     return (
-//       <Wrapper>
-//         <Helmet>
-//           <title>Booking Page</title>
-//           <meta name="Booking Page" content="Forms for booking reservations " />
-//         </Helmet>
-//         <Header />
-//         <BookingForm stage={stage} handleSubmit={this.saveForm} />
-//       </Wrapper>
-//     )
-//   }
-// }
+  render() {
+    const { stage } = this.state
+    return (
+      <Wrapper>
+        <Helmet>
+          <title>Booking Page</title>
+          <meta name="Booking Page" content="Forms for booking reservations " />
+        </Helmet>
+        <Header />
+        <BookingForm stage={stage} handleSubmit={this.saveForm} />
+      </Wrapper>
+    )
+  }
+}
 
-// export const mapDispatchToProps = dispatch => ({
-//   dispatchSaga: () => dispatch(runPostSagaWatcher()),
-//   dispatchOwner: owner => dispatch(postOwner(owner)),
-//   dispatchDog: dog => dispatch(postDog(dog)),
-//   dispatchVet: vet => dispatch(postVet(vet)),
-//   dispatchPath: path => dispatch(setPath(path)),
-// })
+export const mapDispatchToProps = dispatch => ({
+  dispatchSaga: () => dispatch(runPostSagaWatcher()),
+  dispatchOwner: owner => dispatch(postOwner(owner)),
+  dispatchDog: dog => dispatch(postDog(dog)),
+  dispatchVet: vet => dispatch(postVet(vet)),
+  dispatchPath: path => dispatch(setPath(path)),
+})
 
-// const mapStateToProps = createStructuredSelector({
-//   path: makeSelectPath(),
-// })
+const mapStateToProps = createStructuredSelector({
+  path: makeSelectPath(),
+})
 
-// const withConnect = connect(
-//   mapStateToProps,
-//   mapDispatchToProps,
-// )
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)
 
-// const withReducer = injectReducer({ key: 'booking', reducer })
-// const withSaga = injectSaga({ key: 'booking', saga, mode: DAEMON })
+const withReducer = injectReducer({ key: 'booking', reducer })
+const withSaga = injectSaga({ key: 'booking', saga, mode: DAEMON })
 
-// export default compose(
-//   withReducer,
-//   withSaga,
-//   withConnect,
-// )(BookingPage)
+export default compose(
+  withReducer,
+  withSaga,
+  withConnect,
+)(BookingPage)
