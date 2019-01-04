b1d00289f526881ee16028f592fb7584e4aaec49
Ahmad Kayyali <kayyali18@gmail.com>
2019-01-03 21:46:40 -0700

    Create selectors for booking

---
+++ app/containers/BookingPage/reducer.js
@@ -0,0 +1,54 @@
+/*
+ * AppReducer
+ *
+ * The reducer takes care of our data. Using actions, we can change our
+ * application state.
+ * To add a new action, add it to the switch statement in the reducer function
+ *
+ * Example:
+ * case YOUR_ACTION_CONSTANT:
+ *   return state.set('yourStateVariable', true);
+ */
+
+import { fromJS } from 'immutable'
+
+import {
+  POST_VET,
+  POST_DOG,
+  POST_OWNER,
+  POST_SUCCESS,
+  POST_ERROR,
+} from './constants'
+
+// The initial state of the app
+const initialState = fromJS({
+  success: '',
+  error: false,
+  owner: '',
+  dog: '',
+  vet: '',
+})
+
+function bookingReducer(state = initialState, action) {
+  switch (action.type) {
+    case POST_OWNER:
+      return state.set('owner', action.body).set('error', false)
+
+    case POST_DOG:
+      return state.set('dog', action.body).set('error', false)
+
+    case POST_VET:
+      return state.set('vet', action.body).set('error', false)
+
+    case POST_ERROR:
+      return state.set('error', action.error)
+
+    case POST_SUCCESS:
+      return state.set('success', action.message)
+
+    default:
+      return state
+  }
+}
+
+export default bookingReducer
