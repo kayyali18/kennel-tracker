d693e0fb3db4bfad0540ae08a834697e5abacc2f
Ahmad Kayyali <kayyali18@gmail.com>
2019-01-07 20:50:54 -0700

    Successfully post owner, vet, and pet to database.

--- app/containers/BookingPage/index.js
+++ app/containers/BookingPage/index.js
@@ -1,116 +1,134 @@
-// /*
-// * Booking Page
-// * Here we allow clients to book new customers
-// */
+/*
+* Booking Page
+* Here we allow clients to book new customers
+*/
 
-// import React from 'react'
-// import PropTypes from 'prop-types'
-// import { Helmet } from 'react-helmet'
-// import { connect } from 'react-redux'
-// import { compose } from 'redux'
-// import { createStructuredSelector } from 'reselect'
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Helmet } from 'react-helmet'
+import { connect } from 'react-redux'
+import { compose } from 'redux'
+import { createStructuredSelector } from 'reselect'
 
-// import BookingForm from 'components/BookingForm'
-// import injectReducer from 'utils/injectReducer'
-// import injectSaga from 'utils/injectSaga'
-// import { DAEMON } from 'utils/constants'
-// // import Loading from 'components/Loading'
-// import Header from 'components/Header'
-// import saga from './saga'
-// import Wrapper from './Wrapper'
-// import {
-//   runPostSagaWatcher,
-//   postDog,
-//   postOwner,
-//   postVet,
-//   setPath,
-// } from './actions'
-// import reducer from './reducer'
-// import { makeSelectPath } from './selectors'
+import BookingForm from 'components/BookingForm'
+import injectReducer from 'utils/injectReducer'
+import injectSaga from 'utils/injectSaga'
+import { DAEMON } from 'utils/constants'
+// import Loading from 'components/Loading'
+import Header from 'components/Header'
+import saga from './saga'
+import Wrapper from './Wrapper'
+import {
+  runPostSagaWatcher,
+  postDog,
+  postOwner,
+  postVet,
+  setPath,
+} from './actions'
+import reducer from './reducer'
+import {
+  makeSelectPath,
+  makeSelectDog,
+  makeSelectOwner,
+  makeSelectVet,
+} from './selectors'
 
-// /* eslint-disable react/prefer-stateless-function */
+/* eslint-disable react/prefer-stateless-function */
 
-// export class BookingPage extends React.PureComponent {
-//   constructor() {
-//     super()
-//     this.state = {
-//       stage: 1,
-//       stageNames: ['owners', 'pets', 'vets'],
-//       path: 'owners',
-//       dog: {},
-//       owner: {},
-//       vet: {},
-//     }
-//   }
+export class BookingPage extends React.PureComponent {
+  constructor() {
+    super()
+    this.state = {
+      stage: 1,
+      stageNames: ['owners', 'pets', 'vets'],
+      dog: {},
+      owner: {},
+      vet: {},
+    }
+  }
 
-//   componentDidUpdate(prevProps, prevState) {
-//     // Typical usage (don't forget to compare props):
-//     const { dispatchSaga, dispatchOwner, dispatchDog, dispatchVet } = this.props
-//     const { dog, owner, vet } = this.state
+  componentDidUpdate(prevProps, prevState) {
+    const { dispatchSaga, dispatchOwner, dispatchDog, dispatchVet } = this.props
+    const { dog, owner, vet } = this.state
 
-//     if (dog !== prevState.dog) dispatchDog(dog)
-//     if (owner !== prevState.owner) dispatchOwner(owner)
-//     if (vet !== prevState.vet) dispatchVet(vet)
-//     // dispatchSaga()
-//   }
+    if (dog !== prevState.dog) dispatchDog(dog)
+    if (owner !== prevState.owner) dispatchOwner(owner)
+    if (vet !== prevState.vet) dispatchVet(vet)
+    dispatchSaga()
+  }
 
-//   saveForm = inputs => {
-//     const { dispatchSaga, dispatchPath } = this.props
-//     let { stage, stageNames } = this.state
-//     let who
-//     let path = stageNames[stage]
-//     dispatchPath(path)
-//     if (stage === 1) who = 'owner'
-//     if (stage === 2) who = 'dog'
-//     if (stage === 3) who = 'vet'
+  saveForm = inputs => {
+    const { dispatchPath } = this.props
+    const { stageNames } = this.state
+    let { stage } = this.state
+    let who
+    let path = stageNames[stage - 1]
 
-//     stage += 1
-//     path = stageNames[stage]
+    // Update redux store
+    dispatchPath(path)
 
-//     this.setState({
-//       [who]: { ...inputs },
-//       path,
-//       stage,
-//     })
-//   }
+    // who are we updating in state
+    if (stage === 1) who = 'owner'
+    if (stage === 2) who = 'dog'
+    if (stage === 3) who = 'vet'
 
-//   render() {
-//     const { stage } = this.state
-//     return (
-//       <Wrapper>
-//         <Helmet>
-//           <title>Booking Page</title>
-//           <meta name="Booking Page" content="Forms for booking reservations " />
-//         </Helmet>
-//         <Header />
-//         <BookingForm stage={stage} handleSubmit={this.saveForm} />
-//       </Wrapper>
-//     )
-//   }
-// }
+    stage += 1
+    path = stageNames[stage]
 
-// export const mapDispatchToProps = dispatch => ({
-//   dispatchSaga: () => dispatch(runPostSagaWatcher()),
-//   dispatchOwner: owner => dispatch(postOwner(owner)),
-//   dispatchDog: dog => dispatch(postDog(dog)),
-//   dispatchVet: vet => dispatch(postVet(vet)),
-//   dispatchPath: path => dispatch(setPath(path)),
-// })
+    this.setState({
+      [who]: { ...inputs },
+      stage,
+    })
+  }
 
-// const mapStateToProps = createStructuredSelector({
-//   path: makeSelectPath(),
-// })
+  render() {
+    const { stage } = this.state
+    return (
+      <Wrapper>
+        <Helmet>
+          <title>Booking Page</title>
+          <meta name="Booking Page" content="Forms for booking reservations " />
+        </Helmet>
+        <Header />
+        <BookingForm stage={stage} handleSubmit={this.saveForm} />
+      </Wrapper>
+    )
+  }
+}
 
-// const withConnect = connect(
-//   mapStateToProps,
-//   mapDispatchToProps,
-// )
+export const mapDispatchToProps = dispatch => ({
+  dispatchSaga: () => dispatch(runPostSagaWatcher()),
+  dispatchOwner: owner => dispatch(postOwner(owner)),
+  dispatchDog: dog => dispatch(postDog(dog)),
+  dispatchVet: vet => dispatch(postVet(vet)),
+  dispatchPath: path => dispatch(setPath(path)),
+})
 
-// const withReducer = injectReducer({ key: 'booking', reducer })
-// const withSaga = injectSaga({ key: 'booking', saga, mode: DAEMON })
+const mapStateToProps = createStructuredSelector({
+  path: makeSelectPath(),
+  owner: makeSelectOwner(),
+  vet: makeSelectVet(),
+  dog: makeSelectDog(),
+})
 
-// export default compose(
-//   withReducer,
-//   withSaga,
-//   withConnect,
-// )(BookingPage)
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)
+
+const withReducer = injectReducer({ key: 'booking', reducer })
+const withSaga = injectSaga({ key: 'booking', saga, mode: DAEMON })
+
+export default compose(
+  withReducer,
+  withSaga,
+  withConnect,
+)(BookingPage)
+
+BookingPage.propTypes = {
+  dispatchSaga: PropTypes.func,
+  dispatchDog: PropTypes.func,
+  dispatchVet: PropTypes.func,
+  dispatchOwner: PropTypes.func,
+  dispatchPath: PropTypes.func,
+}
--- app/containers/BookingPage/saga.js
+++ app/containers/BookingPage/saga.js
@@ -1,63 +1,72 @@
-// /**
-//  * Gets the suggestions from the kennel_tracker API
-//  */
+/**
+ * Gets the suggestions from the kennel_tracker API
+ */
 
-// import { call, put, takeLatest, select } from 'redux-saga/effects'
+import { call, put, takeLatest, select } from 'redux-saga/effects'
 
-// import { postError, postSuccess } from 'containers/BookingPage/actions'
-// import { RUN_POST_SAGA } from 'containers/BookingPage/constants'
-// import {
-//   makeSelectOwner,
-//   makeSelectDog,
-//   makeSelectVet,
-//   makeSelectPath,
-// } from 'containers/BookingPage/selectors'
-// import request from 'utils/request'
+import { postError, postSuccess } from 'containers/BookingPage/actions'
+import { RUN_POST_SAGA } from 'containers/BookingPage/constants'
+import {
+  makeSelectOwner,
+  makeSelectDog,
+  makeSelectVet,
+  makeSelectPath,
+} from 'containers/BookingPage/selectors'
+import request from 'utils/request'
 
-// /**
-//  * Api request/response handler
-//  */
-// export function* postInfo() {
-//   const path = yield select(makeSelectPath())
-//   const token = JSON.parse(localStorage.getItem('token'))
-//   const body = JSON.stringify({
-//     first_name: 'The',
-//     last_name: 'Dude',
-//     address: '1706 Juniper Dr Los Angeles, LA 13337',
-//     home_phone: '(555) 303 3013',
-//     cell_phone: '(555) 300-3030',
-//     email: 'dude@abides.com',
-//   })
+/**
+ * Api request/response handler
+ */
+export function* postInfo() {
+  const path = yield select(makeSelectPath())
+  const owner = yield select(makeSelectOwner())
+  const dog = yield select(makeSelectDog())
+  const vet = yield select(makeSelectVet())
 
-//   // Post info
-//   const requestURL = `http://kennel-staging.herokuapp.com/api/v1/${path}`
-//   const optionsObj = {
-//     method: 'POST',
-//     headers: {
-//       Authorization: `Bearer ${token}`,
-//       'Content-Type': 'application/json',
-//       Accept: 'application/json',
-//     },
-//     body,
-//   }
-//   // /new reservation
-//   try {
-//     // Call our request helper (see 'utils/request')
-//     const response = yield call(request, requestURL, optionsObj)
+  const body = yield call(getBody, path, owner, dog, vet)
+  const token = JSON.parse(localStorage.getItem('token'))
 
-//     console.log(response)
-//   } catch (err) {
-//     yield put(postError(err))
-//   }
-// }
+  // Post info
+  const requestURL = `http://kennel-staging.herokuapp.com/api/v1/${path}`
+  const optionsObj = {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body,
+  }
+  // /new reservation
+  try {
+    // Call our request helper (see 'utils/request')
+    const response = yield call(request, requestURL, optionsObj)
+    yield call(postSuccess(response))
+  } catch (err) {
+    yield put(postError(err))
+  }
+}
 
-// /**
-//  * Root saga manages watcher lifecycle
-//  */
-// export default function* runPostSaga() {
-//   // Watches for POST_SAGA actions and calls postInfo when one comes in.
-//   // By using `takeLatest` only the result of the latest API call is applied.
-//   // It returns task descriptor (just like fork) so we can continue execution
-//   // It will be cancelled automatically on component unmount
-//   yield takeLatest(RUN_POST_SAGA, postInfo)
-// }
+/**
+ * Root saga manages watcher lifecycle
+ */
+export default function* runPostSaga() {
+  // Watches for POST_SAGA actions and calls postInfo when one comes in.
+  // By using `takeLatest` only the result of the latest API call is applied.
+  // It returns task descriptor (just like fork) so we can continue execution
+  // It will be cancelled automatically on component unmount
+  yield takeLatest(RUN_POST_SAGA, postInfo)
+}
+
+/**
+ *        UTILITY FUNCTIONS
+ *
+ * Functions that are verbose but do important
+ * stuff are kept here. Feel free to contribute!
+ */
+
+function getBody(path, owner, dog, vet) {
+  if (path === 'owners') return JSON.stringify(owner)
+  if (path === 'pets') return JSON.stringify(dog)
+  return JSON.stringify(vet)
+}
